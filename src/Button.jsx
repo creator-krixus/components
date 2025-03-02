4b30c83344036a886efa8d7ee2c03ff414b76111
wilsonRueda <wilson.rueda@onegroupitservices.com>
2025-03-01 19:59:59 -0500

    Fet: add color for props

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,12 +2,13 @@ import PropTypes from 'prop-types';
 import useCount from './store';
 import "./Button.css"
 
-export default function Button({ title = "Click Me" }) {
+export default function Button({ title = "Click Me", color = "red" }) {
   const [state, setState] = useCount();
   return (
     <div>
       <button
         className='shared-btn'
+        style={{ "--btn-color": color }}
         onClick={() => setState((s) => s + 1)}
       >
         {title} {state}
@@ -17,5 +18,6 @@ export default function Button({ title = "Click Me" }) {
 }
 
 Button.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  color: PropTypes.string,
 };
